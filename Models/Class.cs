aac6ce21ffb4aed7e5ad40d88a835fb743c8e0c7
Kajetan Gorski <kajetan.gorski2005@interia.eu>
2024-03-11 09:01:29 +0100

    Initial commit

---
+++ Models/Class.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DrawSystem.Models
+{
+    public class Class
+    {
+        public string ClassName { get; set; }
+        public List<Student> StudentsList { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrawSystem.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Id { get; set; }
+        public bool IsPresent { get; set; } = true;
+    }
+}
